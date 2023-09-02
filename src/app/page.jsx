af111802a25022027c54dd9461b8a35837ebcbbc
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-02 22:35:45 +0700

    homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <main>
-      <div className={styles.container}>Home</div>
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital product.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero_Image" className={styles.img} />
+      </div>
     </main>
   );
 }
