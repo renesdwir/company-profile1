eb3d2d4c4a6e05ae904e7d86240cb2a1e94dd57e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-08 19:26:07 +0700

    blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+            ex ratione laborum laudantium! Nobis omnis, unde dolorem quaerat
+            illum provident, cumque dolore reiciendis vero corporis sapiente
+            modi eaque sunt magnam.
+          </p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+            ex ratione laborum laudantium! Nobis omnis, unde dolorem quaerat
+            illum provident, cumque dolore reiciendis vero corporis sapiente
+            modi eaque sunt magnam.
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
