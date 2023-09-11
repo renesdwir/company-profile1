b220822cc462a4469ecf405eab3aa8870513fdeb
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-11 22:09:00 +0700

    add darkmode toggle navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
+import DarkModeToggle from "../darkmodeToggle/DarkModeToggle";
 const links = [
   { id: 1, title: "Home", url: "/" },
   { id: 2, title: "Portfolio", url: "/portfolio" },
@@ -16,6 +17,7 @@ const Navbar = () => {
         Renesdwir
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
