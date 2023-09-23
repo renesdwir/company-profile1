f65901f1fbf9119869e684fd70c845c2ded44684
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-23 20:49:40 +0700

    post schema init

---
+++ src/models/Post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const { schema } = mongoose;
+const postSchema = new schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", postSchema);
