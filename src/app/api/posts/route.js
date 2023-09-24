787b2c1ef89147eb937282f50cf5e2536ec95312
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-24 22:31:28 +0700

    create api with connection

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,7 +1,15 @@
+import Post from "@/models/Post";
+import connect from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
-  //fetch
-
-  return new NextResponse("Hello World", { status: 200 });
+  try {
+    await connect();
+    const posts = await Post.find();
+    console.log(posts);
+    return new NextResponse(posts, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 };
