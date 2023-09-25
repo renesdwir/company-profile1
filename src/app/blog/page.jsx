0aa6c45718c303fe77bda4169a1de9fa2d90ddfc
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-25 21:14:54 +0700

    fetch data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,29 +2,20 @@ import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
-const Blog = () => {
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+const Blog = async () => {
+  const data = await getData();
+  console.log(data);
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-          <p className={styles.desc}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-            ex ratione laborum laudantium! Nobis omnis, unde dolorem quaerat
-            illum provident, cumque dolore reiciendis vero corporis sapiente
-            modi eaque sunt magnam.
-          </p>
-        </div>
-      </Link>
       <Link href="/blog/testId" className={styles.container}>
         <div className={styles.imageContainer}>
           <Image
