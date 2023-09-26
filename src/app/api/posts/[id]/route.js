1debf50c9757905240f5fe5377fa3cae49e19361
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-26 21:02:05 +0700

    route get by id

---
+++ src/app/api/posts/[id]/route.js
@@ -0,0 +1,15 @@
+import Post from "@/models/Post";
+import connect from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  const { id } = params;
+  try {
+    await connect();
+    const post = await Post.findById(id);
+    return new NextResponse(JSON.stringify(post), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
