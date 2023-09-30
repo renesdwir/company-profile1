9d61c4b412e24e48fbb5ca79a5dda44b9a758194
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-30 21:52:37 +0700

    post route

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,10 +3,25 @@ import connect from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
+  const url = new URL(req.url);
+  const username = url.searchParams.get("username");
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
     return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
+export const POST = async (req) => {
+  const body = await req.json();
+  const newPost = new Post(body);
+
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse("Post has been created!", { status: 201 });
   } catch (error) {
     console.log(error);
     return new NextResponse("Internal Server Error", { status: 500 });
