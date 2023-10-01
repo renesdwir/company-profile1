e63abb4e7ac44394adaca3aa3ff293db68bd0434
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-10-01 20:48:51 +0700

    dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,101 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
+import Image from "next/image";
 const Dashboard = () => {
-  return <div className={styles.container}>Dashboard</div>;
+  const session = useSession();
+  console.log(session);
+  const router = useRouter();
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
+  console.log(data);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const description = e.target[1].value;
+    const image = e.target[2].value;
+    const content = e.target[3].value;
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          description,
+          image,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+  if (session.status === "authenticated") {
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "Loading..."
+            : data.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.image} alt="" width={200} height={100} />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post.id)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form action="" className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input
+            type="text"
+            placeholder="Description"
+            className={styles.input}
+          />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Dashboard;
